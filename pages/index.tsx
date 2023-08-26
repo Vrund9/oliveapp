dad66fbb9a3571fe1268cb0a29f84894ce4e0d0a
Vrund <vrundshah7490@gmail.com>
2023-08-26 13:57:11 +0530

    feat: 🎸 Setup Repo with Tailwind and typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,5 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
-
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
+  <h1>Olive App</h1>
 )
 
 export default IndexPage
