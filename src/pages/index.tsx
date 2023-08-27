a0f62269615bfa1b97607cb587461907b99c033f
Vrund <vrundshah7490@gmail.com>
2023-08-27 14:11:21 +0530

    fix: 🐛 Lazy loading added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
-import Contact from "../components/Contact"
-import Hero from "../components/Hero"
+import React from "react"
+import dynamic from 'next/dynamic'
+
+const DynamicContact = dynamic(() => import('../components/Contact'), {
+  loading: () => <p>Contact Section Loading...</p>,
+})
+
+const DynamicHero = dynamic(() => import('../components/Hero'), {
+  loading: () => <p>Hero Section Loading...</p>,
+})
+
 
 const IndexPage = () => {
   return (
     <>
-    <Hero />
-    <Contact />
+    <DynamicHero />
+    <DynamicContact />
     </>
   )
 }
